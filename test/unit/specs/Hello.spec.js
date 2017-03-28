1a70861e935098e4ce2f9b162fdc2957bee0549f
apple <kyleo.hc@foxmail.com>
2017-03-28 14:45:37 +0800

    update test

--- test/unit/specs/Hello.spec.js
+++ test/unit/specs/Hello.spec.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
-import Hello from '@/components/Hello'
+import Hello from '@/project/demo2/components/Hello'
 
 describe('Hello.vue', () => {
-  it('should render correct contents', () => {
-    const Constructor = Vue.extend(Hello)
-    const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .to.equal('Welcome to Your Vue.js App')
-  })
+    it('should render correct contents', () => {
+        const Constructor = Vue.extend(Hello)
+        const vm = new Constructor().$mount()
+        expect(vm.$el.querySelector('.hello article').textContent)
+            .to.equal('Demo2 work without vue-router')
+    })
 })
