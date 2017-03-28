f6694fe82f183eee2dd46f42b306f7ea17d2ec49
apple <kyleo.hc@foxmail.com>
2017-03-28 10:06:26 +0800

    add sass/scss loader

--- src/project/demo2/components/Hello.vue
+++ src/project/demo2/components/Hello.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="hello">
         <h1>{{ msg }}</h1>
-        <div>Demo2 work without vue-router</div>
+        <article>Demo2 work without vue-router</article>
     </div>
 </template>
 
@@ -15,11 +15,13 @@
         }
     }
 </script>
+<style lang="scss" scoped>
+    $fontSize: 36px;
+    .hello {
+        border: 1px solid #000;
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-    h1 {
-        font-weight: normal;
-        color: green;
+        article {
+            font-size: $fontSize;
+        }
     }
 </style>
