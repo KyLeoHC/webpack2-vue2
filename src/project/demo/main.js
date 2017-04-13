d2df3a1db5c8b10c2be658b124f335e245c04ae8
apple <kyleo.hc@foxmail.com>
2017-04-13 15:21:28 +0800

    add polyfill for 'require.ensure'

--- src/project/demo/main.js
+++ src/project/demo/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './App';
 import routeConfig from './routeConfig';
+import promise from 'es6-promise';
 
+// webpack的require.ensure方法用到浏览器Promise
+// 此处polyfill主要是对IE浏览器的支持
+promise.polyfill();
 Vue.use(VueRouter);
 App.router = new VueRouter(routeConfig);
 /* eslint-disable */
