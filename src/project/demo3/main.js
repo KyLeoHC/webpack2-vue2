eae968f677fb726bd86b7ecc32691fc74808b4f1
apple <kyleo.hc@foxmail.com>
2017-06-06 14:30:05 +0800

    'tree-shaking' test(demo3)

---
+++ src/project/demo3/main.js
@@ -0,0 +1,4 @@
+import * as logger from './utils/logger';
+
+logger.info('This a "info" message!');
+logger.log('This a "log" message!');
---
+++ src/project/demo3/utils/logger.js
@@ -0,0 +1,12 @@
+const info = msg => {
+    console.info(msg);
+};
+
+const log = msg => {
+    console.log(msg);
+};
+
+export {
+    log,
+    info
+};
