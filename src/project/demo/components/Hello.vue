4620131d938c2746ab2d19edef410c805f4a106a
apple <kyleo.hc@foxmail.com>
2017-07-17 15:03:07 +0800

    test async feature

--- src/project/demo/components/Hello.vue
+++ src/project/demo/components/Hello.vue
@@ -2,6 +2,10 @@
     <div class="hello">
         <h1>{{ msg }}</h1>
         <div>Demo1 work with vue-router</div>
+        <div style="color: red;font-size: 18px;"
+             @click="onClick">
+            {{info}}
+        </div>
         <slot></slot>
     </div>
 </template>
@@ -9,10 +13,20 @@
 <script>
     export default {
         name: 'hello',
+        props: {
+            info: {
+                type: String
+            }
+        },
         data() {
             return {
                 msg: 'Welcome to Your Vue.js App'
             };
+        },
+        methods: {
+            onClick() {
+                this.$emit('update:info', 'change info');
+            }
         }
     };
 </script>
--- src/project/demo/views/detail/index.vue
+++ src/project/demo/views/detail/index.vue
@@ -1,17 +1,24 @@
 <template>
     <div>
+        <hello :info.sync="info"></hello>
+        <p style="color: #1609ff">{{info}}</p>
         <p class="detail">message: {{msg}}</p>
         <p>{{detail.text}}</p>
     </div>
 </template>
 <script>
+    import hello from '../../components/Hello.vue';
     import {mapGetters, mapActions} from 'vuex';
 
     export default {
         data() {
             return {
-                msg: ''
+                msg: '',
+                info: 'There is an info'
             };
+        },
+        components: {
+            hello
         },
         created() {
             this.init();
